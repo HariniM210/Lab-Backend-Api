ceef71bc019eecadb7e24029ffa784902939fb28
HariniM210 <harinimanikandan316@gmail.com>
2025-08-08 15:16:19 +0530

    Initial project setup with Node.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+
+const app=express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://harinimanikandan316:<Harini2005>@cluster0.dwmchqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>console.log("MongoDB Atlas connected"))
+.catch(err=>console.log(err));
