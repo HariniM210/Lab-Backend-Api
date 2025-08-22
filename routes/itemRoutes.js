0be0e1b9dbbec3450b4371db9921caa7c24c5552
HariniM210 <harinimanikandan316@gmail.com>
2025-08-22 14:35:44 +0530

    Initial project setup

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -40,6 +40,6 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json({message:err.message});
     }
 });
-
+module.exports = router;
 
 
--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const app=express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://harinimanikandan316:<Harini2005>@cluster0.dwmchqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+mongoose.connect('mongodb+srv://harinimanikandan316:Harini2005@cluster0.dwmchqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
 .then(()=>console.log("MongoDB Atlas connected"))
 .catch(err=>console.log(err));
+const itemRoutes=require('./routes/itemRoutes');
+app.use('/api/items',itemRoutes);
